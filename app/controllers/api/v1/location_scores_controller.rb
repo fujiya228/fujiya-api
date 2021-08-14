d2475d25f32e30b74ef7a913c2843170080d78ec
fujiya228m <fujiya0xE2@gmail.com>
2021-08-15 06:16:56 +0900

    [add] リクエストされた位置にに応じてGoogleMapにリクエストしてレスポンスをそのまま返す

--- app/controllers/api/v1/location_scores_controller.rb
+++ app/controllers/api/v1/location_scores_controller.rb
@@ -8,7 +8,20 @@ module Api::V1
         builder.response :logger # ログを出す
         builder.headers['Content-Type'] = 'application/json' # ヘッダー指定
       end
-      resp = conn.get '/maps/api/place/nearbysearch/json', { location: '35.721183,139.773143', radius: 1500, type: 'restaurant', key: 'AIzaSyBLLxwnSdKnQFDAapcGqMjBhbxz0yUknAg' }
+      
+      # 不正なLocationIDでないかチェック
+      # 緯度：-90~90
+      # 経度：-180~179.999999
+      return render json: {status: 400} if !params[:id].match('_')
+      location_info = params[:id].split('_')
+      latitude = location_info[0].gsub("'", ".").to_f
+      return render json: {status: 400} if latitude < -90 || latitude > 90
+      longitude = location_info[1].gsub("'", ".").to_f
+      return render json: {status: 400} if longitude < -180 || longitude > 179.999999
+      loc = "#{latitude},#{longitude}"
+
+      resp = conn.get '/maps/api/place/nearbysearch/json', { location: loc, radius: 1500, type: 'restaurant', key: 'AIzaSyBLLxwnSdKnQFDAapcGqMjBhbxz0yUknAg' }
+
       render json: { response: JSON.parse(resp.body) }
     end
   end
