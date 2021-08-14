8bbd8ef3b16b88b8cfdda56e6b83c1d58169e4d4
fujiya228m <fujiya0xE2@gmail.com>
2021-08-15 06:01:19 +0900

    [add] Faraday使ってGoogleMapAPIをたたく

--- app/controllers/api/v1/location_scores_controller.rb
+++ app/controllers/api/v1/location_scores_controller.rb
@@ -1,7 +1,15 @@
 module Api::V1
   class LocationScoresController < ApplicationController
     def show
-      render json: { message: "test" }
+      uri = "https://maps.googleapis.com"
+      conn = Faraday::Connection.new(url: uri) do |builder|
+        builder.adapter Faraday.default_adapter
+        builder.request :url_encoded 
+        builder.response :logger # ログを出す
+        builder.headers['Content-Type'] = 'application/json' # ヘッダー指定
+      end
+      resp = conn.get '/maps/api/place/nearbysearch/json', { location: '35.721183,139.773143', radius: 1500, type: 'restaurant', key: 'AIzaSyBLLxwnSdKnQFDAapcGqMjBhbxz0yUknAg' }
+      render json: { response: JSON.parse(resp.body) }
     end
   end
 end
