673d18a263beea017efee26df8a4a9cdee46cced
fujiya228m <fujiya0xE2@gmail.com>
2021-08-15 17:47:27 +0900

    [add] リクエストされた位置に応じてLocationをDBに保存

--- app/controllers/api/v1/location_scores_controller.rb
+++ app/controllers/api/v1/location_scores_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
   class LocationScoresController < ApplicationController
+    before_action :validate_location_id
+    
     def show
       uri = "https://maps.googleapis.com"
       conn = Faraday::Connection.new(url: uri) do |builder|
@@ -8,21 +10,46 @@ module Api::V1
         builder.response :logger # ログを出す
         builder.headers['Content-Type'] = 'application/json' # ヘッダー指定
       end
-      
-      # 不正なLocationIDでないかチェック
-      # 緯度：-90~90
-      # 経度：-180~179.999999
-      return render json: {status: 400} if !params[:id].match('_')
-      location_info = params[:id].split('_')
-      latitude = location_info[0].gsub("'", ".").to_f
-      return render json: {status: 400} if latitude < -90 || latitude > 90
-      longitude = location_info[1].gsub("'", ".").to_f
-      return render json: {status: 400} if longitude < -180 || longitude > 179.999999
-      loc = "#{latitude},#{longitude}"
 
-      resp = conn.get '/maps/api/place/nearbysearch/json', { location: loc, radius: 1500, type: 'restaurant', key: 'AIzaSyBLLxwnSdKnQFDAapcGqMjBhbxz0yUknAg' }
+      location = Location.find_by(location_id: location_id)
+      if location
+        render json: { location: { id: location.id, location_id: location.location_id} }, status: :ok
+      else
+        location = Location.create(location_id: location_id)
+        if location.valid?
+          resp = conn.get '/maps/api/place/nearbysearch/json', { location: location_id, radius: 1500, type: 'restaurant', key: 'AIzaSyBLLxwnSdKnQFDAapcGqMjBhbxz0yUknAg' }
+          render json: { response: JSON.parse(resp.body) }
+        else
+          render json: { messages: location.errors.full_messages }, status: :bad_request
+        end
+      end
+    end
 
-      render json: { response: JSON.parse(resp.body) }
+    def validate_location_id
+      return if /\A\d{1,2}'*\d{0,6}_\d{1,3}'*\d{0,6}\z/.match(params[:id])
+
+      render json: {message: '不正な形式のIDです。'}, status: :bad_request
+    end
+
+    def location_id
+      @location_id ||= point_format(latitude).to_s + ',' + point_format(longitude).to_s
+    end
+
+    def latitude
+      @latitude ||= loc_to_f(params[:id].split('_')[0])
+    end
+    
+    
+    def longitude
+      @longitude ||= loc_to_f(params[:id].split('_')[1])
+    end
+
+    def point_format point
+      point.round(6)
+    end
+
+    def loc_to_f loc
+      loc.gsub("'", ".").to_f
     end
   end
 end
