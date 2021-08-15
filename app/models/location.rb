2bb8638770ec9c88194065f64a2494135d8d6525
fujiya228m <fujiya0xE2@gmail.com>
2021-08-15 17:45:27 +0900

    [add] location model

---
+++ app/models/location.rb
@@ -0,0 +1,53 @@
+class Location < ApplicationRecord
+  # attribute :id, :latitude, :longitude
+  has_many :place
+  has_many :score
+
+  validates :location_id, presence: true, uniqueness: true
+  
+  validate :validate_location_id
+  validate :validate_latitude_range
+  validate :validate_longitude_range
+  
+  before_validation :set_location
+
+  def set_location
+    self.latitude = point_format(latitude)
+    self.longitude = point_format(longitude)
+  end
+
+  def validate_location_id
+    return if /\A\d{1,2}.\d{1,6},\d{1,3}.\d{1,6}\z/.match(self.location_id)
+
+    errors.add(:location_id, '不正な形式のIDです。in model')
+  end
+
+  def validate_latitude_range
+    return if latitude >= -90 && latitude <= 90
+
+    errors.add(:latitude, '範囲外の緯度です。')
+  end
+
+  def validate_longitude_range
+    return if longitude >= -180 && longitude <= 179.999999
+
+    errors.add(:longitude, '範囲外の経度です。')
+  end
+  
+  def latitude
+    @latitude ||= loc_to_f(self.location_id.split(',')[0])
+  end
+  
+  
+  def longitude
+    @longitude ||= loc_to_f(self.location_id.split(',')[1])
+  end
+
+  def point_format point
+    point.round(6)
+  end
+
+  def loc_to_f loc
+    loc.gsub("'", ".").to_f
+  end
+end
